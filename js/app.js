512a9bfff96106625353882a05f2bc3332d1dd1d
Carlos Fins <clockwerkz@gmail.com>
2019-12-18 01:36:50 +0000

    Refactors game logic

--- js/app.js
+++ js/app.js
@@ -1,124 +1,72 @@
 const grid = document.querySelector('.grid');
-const gameGrid = new Array(5);
-for (let i=0; i<gameGrid.length; i++) {
-  gameGrid[i] = [];
-}
+const gameGrid = [
+  [0, 0, 0, 0 ],
+  [0, 0, 0, 0 ],
+  [0, 0, 0, 0 ],
+  [0, 0, 0, 0 ],
+  [0, 0, 0, 0 ]
+];
+
+columnPtr = [0, 0, 0, 0, 0]; //Keeps track of the "height" of each column
 
 grid.addEventListener('click', (e)=>{
   let index  = parseInt(e.target.dataset.value);
   dropNewChip('red', index, e.target);
   let rnd = Math.floor(Math.random()*5);
-  console.log(rnd);
-  let column_pos = rnd+1;
-  let column = document.querySelector(`#col-${column_pos}`);
-  //dropNewChip('yellow', rnd, column);
+  let column = document.querySelector(`#col-${rnd+1}`);
+  dropNewChip('yellow', rnd, column);
 })
 
 function dropNewChip(color, index, column) {
   let chip = document.createElement('div');
   chip.classList.add('chip',`chip--${color}`);
   column.prepend(chip);
-  let y = gameGrid[index].length;
+  let y = columnPtr[index]++;
   gameGrid[index][y] = color;
-  console.log(checkForWin(color,index, y));
+  console.log(checkForWin(color));
 }
 
 
-function checkForWin(color, x, y) {
-  debugger;
-  let colorCount = 0;
-  let ptrX = x;
-  let ptrY = y;
-  
-  //Check from left to right
-  while (gameGrid[ptrX][ptrY] === color && ptrX<gameGrid.length) {
-    colorCount+=1;
-    ptrX+=1;
+function checkForWin(plyr) {
+  //check Horizontally
+  for (let i = 0; i < gameGrid.length; i++) {
+    for (let j=0; j < gameGrid[i].length-3; j++) {
+      if (gameGrid[i][j] === plyr && gameGrid[i][j+1] === plyr && gameGrid[i][j+2] === plyr && gameGrid[i][j+3] === plyr ) {
+        console.log('horizontal',i, j);
+        return true;
+      }
+    }
   }
-  console.log('colorCount when adding', colorCount);
-  if (colorCount === 4) {
-      return true;
-    //game over
+  //check Vertically
+  for (let i = 0; i < gameGrid.length-3; i++) {
+    for (let j=0; j < gameGrid[i].length; j++) {
+      if (gameGrid[i][j] === plyr && gameGrid[i+1][j] === plyr && gameGrid[i+2][j] === plyr && gameGrid[i+3][j] === plyr ) {
+        console.log('vertical', i, j);
+        return true;
+      }
+    }
   }
-  colorCount = 0;
-  ptrX = x;
-  //Check from right to left
-  while (gameGrid[ptrX][ptrY] === color && ptrX>=0) {
-    colorCount+=1;
-    ptrX-=1;
+
+  //upward slope
+  for (let i = gameGrid.length-1; i > 2; i--) {
+    for (let j=0; j < gameGrid[i].length-3; j++) {
+      if (gameGrid[i][j] === plyr && gameGrid[i-1][j+1] === plyr && gameGrid[i-2][j+2] === plyr && gameGrid[i-3][j+3] === plyr ) {
+        console.log('upward slope', i, j);
+        return true;
+      }
+    }
   }
-  console.log('colorCount when subtracting', colorCount);
-  if (colorCount === 4) {
-      return true;
-    //game over
+
+  //downward slope
+  for (let i = 0; i < gameGrid.length-3; i++) {
+    for (let j=0; j < gameGrid[i].length-3; j++) {
+      if (gameGrid[i][j] === plyr && gameGrid[i+1][j+1] === plyr && gameGrid[i+2][j+2] === plyr && gameGrid[i+3][j+3] === plyr ) {
+        console.log('downward slope', i, j);
+        return true;
+      }
+    }
   }
-  colorCount = 0;
-  ptrX = x;
-   //Check from bottom to top
-  while (gameGrid[ptrX][ptrY] === color && ptrY>=0) {
-    colorCount+=1;
-    ptrY-=1;
-  }
-  console.log('colorCount when going from top to bottom', colorCount);
-  if (colorCount === 4) {
-     return true;
-    //game over
-  }
-  ptrX= x;
-  ptrY = y;
-  colorCount=0;
-  //Check diagonally left to right up direction
-  while (gameGrid[ptrX][ptrY] === color && ptrX<gameGrid.length && ptrY<gameGrid[ptrX].length) {
-    colorCount+=1;
-    ptrX+=1;
-    ptrY+=1;
-  }
-  console.log('colorCount when diagonally adding upward', colorCount);
-  if (colorCount === 4) {
-      return true;
-    //game over
-  }
-  ptrX= x;
-  ptrY = y;
-  colorCount=0;
-  //Check diagonally left to right down direction
-  while (gameGrid[ptrX][ptrY] === color && ptrX<gameGrid.length && ptrY>=0) {
-    colorCount+=1;
-    ptrX+=1;
-    ptrY-=1;
-  }
-  console.log('colorCount when diagonally adding downward', colorCount);
-  if (colorCount === 4) {
-      return true;
-    //game over
-  }
-  ptrX= x;
-  ptrY = y;
-  colorCount=0;
-  //Check diagonally right to left downward
-  while (gameGrid[ptrX][ptrY] === color && ptrX>=0 && ptrY>=0) {
-    colorCount+=1;
-    ptrX-=1;
-    ptrY-=1;
-  }
-  console.log('colorCount when diagonally down subtracting', colorCount);
-  if (colorCount === 4) {
-      return true;
-    //game over
-  }
-  ptrX= x;
-  ptrY = y;
-  colorCount=0;
-  //Check diagonally right to left upward
-  while (gameGrid[ptrX][ptrY] === color && ptrX>=0 && ptrY<gameGrid[ptrX].length) {
-    colorCount+=1;
-    ptrX-=1;
-    ptrY+=1;
-  }
-  //console.log('colorCount when diagonally up subtracting', colorCount);
-  if (colorCount === 4) {
-      return true;
-    //game over
-  }
+  return false;
 }
+
 
